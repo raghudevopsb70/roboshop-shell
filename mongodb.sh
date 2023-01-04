2cd7eed7e636b0c0afc770c715d0b4e537a53f0f
Raghu K <raghuk.vit@gmail.com>
2023-01-04 06:54:39 +0530

    Roboshop Frontend Script

---
+++ mongodb.sh
@@ -0,0 +1,10 @@
+script_location=$(pwd)
+
+cp ${script_location}/files/mongodb.repo /etc/yum.repos.d/mongodb.repo
+
+yum install mongodb-org -y
+
+systemctl enable mongod
+systemctl start mongod
+
+
