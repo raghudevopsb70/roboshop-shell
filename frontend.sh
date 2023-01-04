0709e984d889852737f992a17066002a56235878
Raghu K <raghuk.vit@gmail.com>
2023-01-04 06:57:33 +0530

    MongoDB Script

--- frontend.sh
+++ frontend.sh
@@ -1,8 +1,6 @@
 script_location=$(pwd)
 
 yum install nginx -y
-systemctl enable nginx
-systemclt start nginx
 rm -rf /usr/share/nginx/html/*
 
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
@@ -11,4 +9,6 @@ unzip /tmp/frontend.zip
 
 cp ${script_location}/files/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
 
+
+systemctl enable nginx
 systemctl restart nginx
--- mongodb.sh
+++ mongodb.sh
@@ -4,7 +4,9 @@ cp ${script_location}/files/mongodb.repo /etc/yum.repos.d/mongodb.repo
 
 yum install mongodb-org -y
 
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+
 systemctl enable mongod
-systemctl start mongod
+systemctl restart mongod
 
 
