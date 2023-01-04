8f22591519acee054eb818b27b28276b886f8e89
Raghu K <raghuk.vit@gmail.com>
2023-01-04 07:24:21 +0530

    Catalogue Script

--- frontend.sh
+++ frontend.sh
@@ -1,14 +1,24 @@
 script_location=$(pwd)
 
+echo -e "\e[35m Install Nginx\e[0m"
 yum install nginx -y
+
+echo -e "\e[35m Remove Nginx Old Content\e[0m"
 rm -rf /usr/share/nginx/html/*
 
+echo -e "\e[35m Download Frontend Content \e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+
 cd /usr/share/nginx/html
+
+echo -e "\e[35m Extract Frontend Content\e[0m"
 unzip /tmp/frontend.zip
 
+echo -e "\e[35m Copy RoboShop Nginx Config File \e[0m"
 cp ${script_location}/files/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
 
-
+echo -e "\e[35m Enable Nginx\e[0m"
 systemctl enable nginx
+
+echo -e "\e[35m Start Nginx\e[0m"
 systemctl restart nginx
