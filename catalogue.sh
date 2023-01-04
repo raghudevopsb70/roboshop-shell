b9fdfea43302da77d23214c7624558ba61dfc019
Raghu K <raghuk.vit@gmail.com>
2023-01-04 07:13:12 +0530

    Catalogue Script

--- catalogue.sh
+++ catalogue.sh
@@ -6,6 +6,7 @@ yum install nodejs -y
 #useradd roboshop
 mkdir -p /app
 curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
+rm -rf /app/*
 cd /app
 unzip /tmp/catalogue.zip
 cd /app
