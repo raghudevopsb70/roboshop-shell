200458abd5aef5c90f80932e315451a6cdd586c0
Raghu K <raghuk.vit@gmail.com>
2023-01-05 07:08:58 +0530

    Update roboshop-shell/

--- catalogue.sh
+++ catalogue.sh
@@ -19,5 +19,5 @@ systemctl start catalogue
 cp ${script_location}/files/mongodb.repo /etc/yum.repos.d/mongodb.repo
 yum install mongodb-org-shell -y
 
-mongo --host localhost </app/schema/catalogue.js
+mongo --host mongodb-dev.devopsb70.online </app/schema/catalogue.js
 
