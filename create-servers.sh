3484597e2b5616d822d3b303c0c5b1e68fa619fe
Raghu K <raghuk.vit@gmail.com>
2023-01-05 06:41:02 +0530

    Create Servers script

---
+++ create-servers.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+##### Change these values ###
+ZONE_ID="Z0366464237Z7LZLZPKFA"
+DOMAIN="devopsb70.online"
+SG_NAME="allow-all"
+env=dev
+#############################
+
+
+
+create_ec2() {
+  PRIVATE_IP=$(aws ec2 run-instances \
+      --image-id ${AMI_ID} \
+      --instance-type t3.micro \
+      --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}, {Key=Monitor,Value=yes}]" "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=${COMPONENT}}]"  \
+      --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}"\
+      --security-group-ids ${SGID} \
+      --iam-instance-profile Name=aws_ssm_dev_role \
+      | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
+
+  sed -e "s/IPADDRESS/${PRIVATE_IP}/" -e "s/COMPONENT/${COMPONENT}/" -e "s/DOMAIN/${DOMAIN}/" route53.json >/tmp/record.json
+  aws route53 change-resource-record-sets --hosted-zone-id ${ZONE_ID} --change
+  -batch file:///tmp/record.json | jq
+}
+
+
+## Main Program
+AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-8-DevOps-Practice" | jq '.Images[].ImageId' | sed -e 's/"//g')
+if [ -z "${AMI_ID}" ]; then
+  echo "AMI_ID not found"
+  exit 1
+fi
+
+SGID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=${SG_NAME} | jq  '.SecurityGroups[].GroupId' | sed -e 's/"//g')
+if [ -z "${SGID}" ]; then
+  echo "Given Security Group does not exit"
+  exit 1
+fi
+
+
+for component in catalogue cart user shipping payment frontend mongodb mysql rabbitmq redis dispatch; do
+  COMPONENT="${component}-${env}"
+  create_ec2
+done
