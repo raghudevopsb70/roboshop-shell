deb917e6ff512faeee7587197f808be2051f699e
Raghu K <raghuk.vit@gmail.com>
2023-01-05 07:23:59 +0530

    Update roboshop-shell/

--- frontend.sh
+++ frontend.sh
@@ -1,24 +1,26 @@
 script_location=$(pwd)
+LOG=/tmp/roboshop.log
 
 echo -e "\e[35m Install Nginx\e[0m"
-yum install nginx -y
+yum install nginx -y &>>${LOG}
 
 echo -e "\e[35m Remove Nginx Old Content\e[0m"
-rm -rf /usr/share/nginx/html/*
+rm -rf /usr/share/nginx/html/* &>>${LOG}
 
 echo -e "\e[35m Download Frontend Content \e[0m"
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${LOG}
 
-cd /usr/share/nginx/html
+cd /usr/share/nginx/html &>>${LOG}
 
 echo -e "\e[35m Extract Frontend Content\e[0m"
-unzip /tmp/frontend.zip
+unzip /tmp/frontend.zip &>>${LOG}
 
 echo -e "\e[35m Copy RoboShop Nginx Config File \e[0m"
-cp ${script_location}/files/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
+cp ${script_location}/files/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${LOG}
 
 echo -e "\e[35m Enable Nginx\e[0m"
-systemctl enable nginx
+systemctl enable nginx &>>${LOG}
 
 echo -e "\e[35m Start Nginx\e[0m"
-systemctl restart nginx
+systemctl restart nginx &>>${LOG}
+
