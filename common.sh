92d0403a46f589929339ca0a75606345371bcf13
Raghu K <raghuk.vit@gmail.com>
2023-01-06 06:51:21 +0530

    Update roboshop-shell/

--- common.sh
+++ common.sh
@@ -3,9 +3,9 @@ LOG=/tmp/roboshop.log
 
 status_check() {
   if [ $? -eq 0 ]; then
-    echo -e "\e[32mSUCCESS\e[0m"
+    echo -e "\e[1;32mSUCCESS\e[0m"
   else
-    echo -e "\e[31mFAILURE\e[0m"
+    echo -e "\e[1;31mFAILURE\e[0m"
     echo "Refer Log file for more information, LOG - ${LOG}"
     exit
   fi
