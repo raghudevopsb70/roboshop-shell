caed2297889a63d0759c5c61909c0f6480f15f94
Raghu K <raghuk.vit@gmail.com>
2023-01-07 06:19:38 +0530

    Update roboshop-shell/

--- common.sh
+++ common.sh
@@ -14,3 +14,68 @@ status_check() {
 print_head() {
   echo -e "\e[1m $1 \e[0m"
 }
+
+NODEJS() {
+  print_head "Configuring NodeJS Repos"
+  curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${LOG}
+  status_check
+
+  print_head "Install NodeJS"
+  yum install nodejs -y &>>${LOG}
+  status_check
+
+  print_head "Add Application User"
+  id roboshop &>>${LOG}
+  if [ $? -ne 0 ]; then
+    useradd roboshop &>>${LOG}
+  fi
+  status_check
+
+  mkdir -p /app &>>${LOG}
+
+  print_head "Downloading App content"
+  curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${LOG}
+  status_check
+
+  print_head "Cleanup Old Content"
+  rm -rf /app/* &>>${LOG}
+  status_check
+
+  print_head "Extracting App Content"
+  cd /app
+  unzip /tmp/${component}.zip &>>${LOG}
+  status_check
+
+  print_head "Installing NodeJS Dependencies"
+  cd /app &>>${LOG}
+  npm install &>>${LOG}
+  status_check
+
+  print_head "Configuring ${component} Service File"
+  cp ${script_location}/files/${component}.service /etc/systemd/system/${component}.service &>>${LOG}
+  status_check
+
+  print_head "Reload SystemD"
+  systemctl daemon-reload &>>${LOG}
+  status_check
+
+  print_head "Enable ${component} Service "
+  systemctl enable ${component} &>>${LOG}
+  status_check
+
+  print_head "Start ${component} service "
+  systemctl start ${component} &>>${LOG}
+  status_check
+
+  print_head "Configuring Mongo Repo "
+  cp ${script_location}/files/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>${LOG}
+  status_check
+
+  print_head "Install Mongo Client"
+  yum install mongodb-org-shell -y &>>${LOG}
+  status_check
+
+  print_head "Load Schema"
+  mongo --host mongodb-dev.devopsb70.online </app/schema/${component}.js &>>${LOG}
+  status_check
+}
--- user.sh
+++ user.sh
@@ -1,5 +1,5 @@
 source common.sh
 
-component=catalogue
+component=user
 
 NODEJS
